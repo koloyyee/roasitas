ed583618cb83c26ee09c3e5c74cb7359bd9a794c
Loy Yee Ko <koloyyee@gmail.com>
2020-01-31 16:45:41 +0800

    changed urls for user

--- backend/project_roasitas/urls.py
+++ backend/project_roasitas/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.models import User
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
@@ -34,19 +34,24 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
-             template_name='users/password_reset.html'
+            template_name='users/password_reset.html',
+            subject_template_name='users/password_reset_subject.txt',
+            success_url=reverse_lazy('users:password_reset_done')
          ),
          name='password_reset'),
+
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='users/password_reset_done.html'
          ),
          name='password_reset_done'),
+
     path('password-reset-confirm/<uidb64>/<token>/',
     auth_views.PasswordResetConfirmView.as_view(
         template_name='users/password_reset_confirm.html'
         ),
     name='password_reset_confirm'),
+
      path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name='users/password_reset_complete.html'
