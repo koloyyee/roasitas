9577e7379c6b22328341abde8755736967f59180
Loy Yee Ko <koloyyee@gmail.com>
2020-02-01 10:26:54 +0800

    test for commenting password_reset on login.html

--- backend/project_roasitas/urls.py
+++ backend/project_roasitas/urls.py
@@ -34,9 +34,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
-            template_name='users/password_reset.html',
-            subject_template_name='users/password_reset_subject.txt',
-            success_url=reverse_lazy('users:password_reset_done')
+            template_name='users/password_reset.html'
          ),
          name='password_reset'),
 
