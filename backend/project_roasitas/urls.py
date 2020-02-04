829fd01be6884c79dbdacdc5705a7e2423e3efc3
Loy Yee Ko <koloyyee@gmail.com>
2020-02-04 20:44:00 +0800

    url error, should have changed before the last commit

--- backend/project_roasitas/urls.py
+++ backend/project_roasitas/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
     path('profile/', users_view.profile, name='profile'),
     path('accounts/', include('django.contrib.auth.urls')),
     
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('password_reset/',
     #      auth_views.PasswordResetView.as_view(
     #         template_name='users/password_reset.html'
